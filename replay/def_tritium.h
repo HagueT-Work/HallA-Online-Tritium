e400b242571e634bc5f9b9ff3dc7c34e0a0bc63b
Tyler Hague <tjhague@jlab.org>
2017-09-29 14:30:14 -0400

    set paths

--- replay/def_tritium.h
+++ replay/def_tritium.h
@@ -52,7 +52,7 @@ typedef struct _sReplaySetUp
 //////////////////////////////////////////////////////////////////
 
 static const char* PATHS[] = {
-  "/home/jbane/raw",
+  "/home/me/raw",
   "/adaq1/data1",
   "/adaql2/data1",
   "/adaql2/data2",
