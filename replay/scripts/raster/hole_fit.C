00ba3fd35e594f692cabaca74b233604ed1c4fea
Tyler Hague <TJHague@gmail.com>
2018-07-11 22:26:53 -0400

    Updated beam position for raster calibration
    Beam is now centered at the projection of the BPMs to the target rather than centering the carbon hole at (0,0).

--- replay/scripts/raster/hole_fit.C
+++ replay/scripts/raster/hole_fit.C
@@ -89,7 +89,7 @@ void hole_fit(){
 
   Double_t param[6] = {25,0.00008,72500,0.00003,80000, 1.};
   ell_fit->SetParameters(param);
-  ell_fit->SetParLimits(0,10,50);
+  ell_fit->SetParLimits(0,3,50);
   ell_fit->SetParLimits(1,.00006,.0001);
   ell_fit->SetParLimits(2,65000,75000);
   ell_fit->SetParLimits(3,.00001,.00005);
@@ -100,15 +100,15 @@ void hole_fit(){
 
   Double_t param2[6] = {25,0.00008,72500,0.00003,80000, 1.};
   ell_fit2->SetParameters(param2);
-  ell_fit2->SetParLimits(0,10,50);
+  ell_fit2->SetParLimits(0,3,50);
   ell_fit2->SetParLimits(1,.00006,.0001);
   ell_fit2->SetParLimits(2,65000,75000);
   ell_fit2->SetParLimits(3,.00001,.00005);
   ell_fit2->SetParLimits(4,70000,85000);
   ell_fit2->SetParLimits(5,.5,10);
 
-  TH2F *R1 = new TH2F("R1","Raster 1 Carbon Hole",25,45000,95000,50,20000,120000);
-  TH2F *R2 = new TH2F("R2","Raster 2 Carbon Hole",25,45000,95000,50,20000,120000);
+  TH2F *R1 = new TH2F("R1","Raster 1 Carbon Hole",70,45000,95000,50,20000,120000);
+  TH2F *R2 = new TH2F("R2","Raster 2 Carbon Hole",70,45000,95000,50,20000,120000);
 
   TCanvas *c1 = new TCanvas();
   TCanvas *c2 = new TCanvas();
@@ -120,8 +120,106 @@ void hole_fit(){
   rootfile->Draw(arm + ".Raster2.rawcur.y:" + arm + ".Raster2.rawcur.x>>R2",TCut(cut),"colz");
   R2->Fit(ell_fit2);
   //ell_fit2->Draw("cont1 same");
-  
-  cout << arm << ".Raster.raw2posT = " << ell_fit->GetParameter(2)*-1.*kx*ell_fit->GetParameter(1)/1000. << " " << ell_fit->GetParameter(4)*-1.*ky*ell_fit->GetParameter(3)/1000. << " " << ell_fit->GetParameter(1)*kx/1000. << " " << ell_fit->GetParameter(3)*ky/1000. << " 0.0 0.0" << endl;
 
-  cout << arm << ".Raster2.raw2posT = " << ell_fit2->GetParameter(2)*-1.*kx*ell_fit2->GetParameter(1)/1000. << " " << ell_fit2->GetParameter(4)*-1.*ky*ell_fit2->GetParameter(3)/1000. << " " << ell_fit2->GetParameter(1)*kx/1000. << " " << ell_fit2->GetParameter(3)*ky/1000. << " 0.0 0.0" << endl;
+  //***************************************************************************
+  //Now plotting the raster in 1D as well as the projection of the BPMs to the 
+  //target. Previously this code set the center of the carbon hole to (0,0,0).
+  //Further investigation has revealed this to be the wrong way to go about it.
+  //The average position of the raster should be set to the average of the
+  //projection of the BPMs to the target.
+  //
+  //This is ideally done with a carbon foil run as it has no features to bias
+  //the raster spectrum (e.g. a 2mm hole) and is not susceptible to beam
+  //heating affects. Alternatively, the clock trigger can be used.
+  //***************************************************************************
+
+  Int_t run2 = 0;
+  cout << "What run number would you like to calibrate with?    ";
+  cin >> run2;
+  cout << endl << endl;
+  if(run2<=0){
+    cout << "Invalid run number. Exiting." << endl << endl;
+    return;
+  }
+
+  TChain *foilrun = new TChain("T");
+
+  int j = 1;
+
+  if(!gSystem->AccessPathName(TString::Format("/cache/halla/triton/prod/marathon/pass1_calibration/kin1/tritium_%d.root",run2),kFileExists)){
+    foilrun->Add(TString::Format("/cache/halla/triton/prod/marathon/pass1_calibration/kin1/tritium_%d.root",run2));
+    cout << "Added file: tritium_" << run2 << ".root" << endl;
+  }else{
+    cout << "Requested run has not been replayed. Exiting." << endl << endl;
+    return;
+  }
+
+  while(!gSystem->AccessPathName(TString::Format("/cache/halla/triton/prod/marathon/pass1_calibration/kin1/tritium_%d_%d.root",run2,j),kFileExists)){
+    foilrun->Add(TString::Format("/cache/halla/triton/prod/marathon/pass1_calibration/kin1/tritium_%d_%d.root",run2,j));
+    cout << "Added file: tritium_" << run2 << "_" << j << ".root" << endl;
+    j=j+1;
+  }                      
+
+  //Create strings that will get the position of the beam at the target from BPM info
+  //Hardcoded numbers are the position of BPMB w.r.t. the target and the distance between the two BPMs
+  TString targx_string = "(" + arm + ".BPMB.x) + ((2.214) * ((" + arm + ".BPMB.x - " + arm + ".BPMA.x) / (5.131)))";
+  TString targy_string = "(" + arm + ".BPMB.y) + ((2.214) * ((" + arm + ".BPMB.y - " + arm + ".BPMA.y) / (5.131)))";
+
+  //Plot X and Y Currents for both Rasters
+  TH1F *r1xcurr = new TH1F("r1xcurr", "Raster 1-X Current vs ADC Channel", 1000, 45000, 95000);
+  TH1F *r1ycurr = new TH1F("r1ycurr", "Raster 1-Y Current vs ADC Channel", 1000, 20000, 120000);
+  TH1F *r2xcurr = new TH1F("r2xcurr", "Raster 2-X Current vs ADC Channel", 1000, 45000, 95000);
+  TH1F *r2ycurr = new TH1F("r2ycurr", "Raster 2-Y Current vs ADC Channel", 1000, 20000, 120000);
+  
+  //Plot X and Y Position at the Target
+  TH1F *targxpos = new TH1F("targxpos", "Target X Position from BPMs (m)", 400, -0.02, 0.02);
+  TH1F *targypos = new TH1F("targypos", "Target Y Position from BPMs (m)", 400, -0.02, 0.02);
+
+  TCanvas *meanC = new TCanvas("Mean Canvas");
+  meanC->Divide(2,3);
+
+  //Using clock trigger cut
+  TCut clock;
+  if(RIGHT_ARM_CONDITION){
+    clock = "DR.evtypebits>>8&1";
+  }else if(LEFT_ARM_CONDITION){
+    clock = "DL.evtypebits>>8&1";
+  }
+  if(RIGHT_ARM_CONDITION){
+    clock += "&&evRight";
+  }else if(LEFT_ARM_CONDITION){
+    cut += "&&evLeft";
+  }
+  cut += "dnew_r*0.0003299)>18)";
+
+  //Draw the histograms
+  //It is easy to ge the mean when put into a histogram
+  //The plots should also be checked to ensure that the entire spectra is
+  //included in the plot. If not, the means will be thrown off.
+  meanC->cd(1);
+  foilrun->Draw(arm+".Raster.rawcur.x>>r1xcurr",clock); 
+  meanC->cd(2);
+  foilrun->Draw(arm+".Raster.rawcur.y>>r1ycurr",clock);
+  meanC->cd(3);
+  foilrun->Draw(arm+".Raster2.rawcur.x>>r2xcurr",clock);
+  meanC->cd(4);
+  foilrun->Draw(arm+".Raster2.rawcur.y>>r2ycurr",clock);
+  meanC->cd(5);
+  foilrun->Draw(targx_string + ">>targxpos",clock);
+  meanC->cd(6);
+  foilrun->Draw(targy_string + ">>targypos",clock);
+
+  Double_t r1xm, r1ym, r2xm, r2ym, txm, tym;
+  r1xm = r1xcurr->GetMean();
+  r1ym = r1ycurr->GetMean();
+  r2xm = r2xcurr->GetMean();
+  r2ym = r2ycurr->GetMean();
+  txm = targxpos->GetMean();
+  tym = targypos->GetMean();
+
+  //cout << std::setprecision(9) << r1xm << " " << r1ym << " " << r2xm << " " << r2ym << endl << endl; 
+ 
+  cout << arm << ".Raster.raw2posT = " << txm-r1xm*kx*ell_fit->GetParameter(1)/1000. << " " << tym-r1ym*ky*ell_fit->GetParameter(3)/1000. << " " << ell_fit->GetParameter(1)*kx/1000. << " " << ell_fit->GetParameter(3)*ky/1000. << " 0.0 0.0" << endl;
+
+  cout << arm << ".Raster2.raw2posT = " << txm-r2xm*kx*ell_fit2->GetParameter(1)/1000. << " " << tym-r2ym*ky*ell_fit2->GetParameter(3)/1000. << " " << ell_fit2->GetParameter(1)*kx/1000. << " " << ell_fit2->GetParameter(3)*ky/1000. << " 0.0 0.0" << endl;
 }
